db487e102e28c8ce12d6e74137a585de4b4c00bb
Curly-stasya <st095281@student.spbu.ru>
2021-10-11 00:22:40 +0300

    3

---
+++ stars.2.0.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal  enabledelayedexpansion
+
+set string=
+
+for /L %%i in (%1,-1,1) do (
+	for /L %%k in (1,1,%%i) do (
+		set string=!string!*
+	)
+	echo !string!
+	set string=
+)
