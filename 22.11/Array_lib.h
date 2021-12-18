6510ff86ac998cde1a4b9f1391a30dcfcd1b80db
Curly-stasya <91792338+Curly-stasya@users.noreply.github.com>
2021-12-18 10:12:14 +0300

    Add files via upload

---
+++ 22.11/Array_lib.h
@@ -0,0 +1,9 @@
+#ifndef ARRAY_LIB
+#define ARRAY_LIB
+
+int** Array2d(int rows, int colms, int C);
+int** EmptyArray2d(int rows, int colms);
+void output(int** array, int rows, int colms);
+void cleanArray2d(int** array, int rows);
+
+#endif 
