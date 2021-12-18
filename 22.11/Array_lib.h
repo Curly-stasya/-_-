e606673ed9dd9a392627d1cec927f323f161bebb
Curly-stasya <st095281@student.spbu.ru>
2021-12-18 14:13:29 +0300

    task

---
+++ 22.11/Array_lib.h
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+#ifndef ARRAY_LIB
+#define ARRAY_LIB
+
+int** Array2d(int rows, int colms, int C);
+int** EmptyArray2d(int rows, int colms);
+void output(int** array, int rows, int colms);
+void cleanArray2d(int** array, int rows);
+
+=======
+#ifndef ARRAY_LIB
+#define ARRAY_LIB
+
+int** Array2d(int rows, int colms, int C);
+int** EmptyArray2d(int rows, int colms);
+void output(int** array, int rows, int colms);
+void cleanArray2d(int** array, int rows);
+
+>>>>>>> 7e9185ff8767bed22a0b418181f77bbbb9fa8079
+#endif 
